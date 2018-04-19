5bcebf220b8b2e09a11c1713e15c68476f6220a8
AReinold <anneke.reinold@tu-dortmund.de>
2018-04-20 01:00:30 +0200

    Anfang Auswertung/ Volumenberechnung

---
+++ V70 Vakuumphysik/python-Berechnung/volumen.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import uncertainties.unumpy as unp
+import scipy.constants as const
+from scipy.optimize import curve_fit
+from uncertainties import correlated_values, correlation_matrix
+from uncertainties import ufloat
+from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
