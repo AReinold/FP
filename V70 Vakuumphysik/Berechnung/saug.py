be32422d8725733dc4eff96f0244ad19c1bd4759
AReinold <anneke.reinold@tu-dortmund.de>
2018-06-10 20:20:50 +0200

    Gegengelesen und rausgeschickt :squirrel:

--- V70 Vakuumphysik/Berechnung/saug.py
+++ V70 Vakuumphysik/Berechnung/saug.py
@@ -7,9 +7,9 @@ from uncertainties import correlated_values, correlation_matrix
 from uncertainties import ufloat
 from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
 
-p=ufloat(0.0002, 0.00002)
+p=ufloat(0.0001, 0.00001)
 V=ufloat(10.0,0.8)
-m=ufloat(0.000436, 0.000012)
+m=ufloat(0.000186, 0.000007)
 S=(V/p)*m
 
 print(S)
